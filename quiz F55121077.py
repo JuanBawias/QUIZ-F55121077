a3f422a6c856fbce888277ca1d389abc695f1bf2
JuanBawias <114935957+JuanBawias@users.noreply.github.com>
2022-12-13 21:59:11 +0800

    Add files via upload

--- quiz F55121077.py
+++ quiz F55121077.py
@@ -1,5 +1,13 @@
-y= []
-
-for x in range(0,20,3):
-    if(x > 0):
-        y.append(x)
+sumofmultipe = []
+a = 3
+while a < 20:
+    if a % 3 == 0:
+        sumofmultipe.append(a)
+    elif a & 5 == 0:
+        sumofmultipe.append(a)
+    a = a + 3
+print (sumofmultipe)
+sums = 0
+for i in sumofmultipe:
+    sums = sums + i
+print (sums)
